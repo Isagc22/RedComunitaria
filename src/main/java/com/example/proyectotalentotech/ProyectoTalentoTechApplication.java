76e23119edb5ef4f328fd3781e832204b336066d
Jorge Alvarado <jalvaradocuesta@gmail.com>
2025-03-15 11:35:47 -0500

    .

---
+++ src/main/java/com/example/proyectotalentotech/ProyectoTalentoTechApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectotalentotech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoTalentoTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoTalentoTechApplication.class, args);
+    }
+
+}
