0c880ac508a49331ac0b69b486c69f0e4e79e771
Jorge Alvarado <jalvaradocuesta@gmail.com>
2025-03-30 10:52:48 -0500

    registro

--- src/main/java/com/example/proyectotalentotech/ProyectoTalentoTechApplication.java
+++ src/main/java/com/example/proyectotalentotech/ProyectoTalentoTechApplication.java
@@ -2,11 +2,14 @@ package com.example.proyectotalentotech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 
 public class ProyectoTalentoTechApplication {
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(ProyectoTalentoTechApplication.class, args);
